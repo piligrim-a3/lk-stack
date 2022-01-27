0afca79c2c80a14c745b3833dbfeaf4bb7be3232
out <piligrim.a3@gmail.com>
2022-01-27 11:44:51 +0900

    add: LkList init

---
+++ src/main/java/ru/bgpu/lk/stack/LkStack.java
@@ -0,0 +1,31 @@
+package ru.bgpu.lk.stack;
+
+public class LkStack <T> {
+
+    private T[] array;
+    private int current;
+
+    LkStack(int size) {
+
+    }
+
+    private void push(T value) {
+
+    }
+
+    public T peek() {
+        return null;
+    }
+
+    public T pull() {
+        return null;
+    }
+
+    public boolean isEmpty() {
+        return true;
+    }
+
+    public int size() {
+        return 0;
+    }
+}
